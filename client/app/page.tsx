4c5c7f7bb6fe3e49354b002751671132a44db819
Simran <btech10760.21@bitmesra.ac.in>
2025-06-06 11:00:45 +0530

    feat: initialize server with essential routes and services
    - Added package.json for server dependencies and scripts.
    - Implemented analytics routes for user, order, and course analytics.
    - Created course management routes including CRUD operations.
    - Developed layout and notification routes for admin functionalities.
    - Established order processing routes for creating and managing orders.
    - Integrated user authentication and management routes.
    - Set up server entry point with Cloudinary configuration and socket server initialization.
    - Developed services for course and order management with error handling.
    - Implemented user service for managing user data and roles.
    - Created utility functions for error handling, JWT management, Redis integration, and email sending.
    - Configured TypeScript settings for the server.
    - Established MongoDB connection utility for database interactions.
    - Added analytics data generation utility for reporting.

---
+++ client/app/page.tsx
@@ -0,0 +1,34 @@
+'use client'
+import React, { FC, useState } from "react"
+import Heading from "./utils/Heading";
+import Header from "./components/Header";
+import { open } from "fs";
+import Hero from "./components/Route/Hero"
+
+interface Props {}
+
+const Page: FC<Props> =(props)=> {
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(0);
+  const [route, setRoute] = useState("Login")
+
+  return(
+    <div>
+      <Heading 
+      title={"Elearning"} 
+      description={"Elearning is a platform for students to learn and get help from teachers "} 
+      keywords={"Programming, MERN, Redux"} 
+      />
+      <Header 
+      open={open} 
+      setOpen={setOpen}
+      activeItem={activeItem} 
+      setRoute={setRoute}
+      route={route }
+      />
+      <Hero /> 
+    </div>
+  )
+};
+
+export default Page;
